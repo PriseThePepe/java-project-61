839d46829378da79b0ad43a44295b3ec202b8271
Ivan Piglov <famenko72@gmail.com>
2024-03-21 21:27:19 +0300

    fix App.java

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -1,8 +1,48 @@
 package hexlet.code;
 
+
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.congratCli();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner choice = new Scanner(System.in);
+        int choiceNb = Integer.parseInt(choice.next());
+        switch (choiceNb) {
+            case (0):
+                System.out.println("Bye-bye");
+                break;
+            case (1):
+                Cli.greetCli();
+                break;
+            case (2):
+                Engine.playEvenGame();
+                break;
+            case (3):
+                Engine.playCalcGame();
+                break;
+            case(4):
+                Engine.playGCDGame();
+                break;
+            case(5):
+                Engine.playProgression();
+                break;
+            case(6):
+                Engine.playPrime();
+                break;
+            default:  break;
+
+
+
+        }
     }
 }
