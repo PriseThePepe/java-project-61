14ea9475440681a2b3dda7630413f30147767cd8
Ivan Piglov <famenko72@gmail.com>
2024-03-21 21:29:22 +0300

    fix GCDGame.java

--- app/src/main/java/hexlet/code/games/GCDGame.java
+++ app/src/main/java/hexlet/code/games/GCDGame.java
@@ -4,36 +4,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GCDGame {
-    public void GCDgame(String cliName){
+    public void gcdGame(String cliName) {
         Random random = new Random();
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int correctCount = 0;
         while (correctCount < 3) {
             int rnNumbOne = random.nextInt(1, 100);
             int rnNumbTwo = random.nextInt(1, 100);
             int max;
-            System.out.println("Question: " + rnNumbOne +" "+ rnNumbTwo);
-            String answer =scanner.next();
-            int numAnswer=0;
-            if(answer.matches("-?\\d+")){
+            System.out.println("Question: " + rnNumbOne + " " + rnNumbTwo);
+            System.out.print("Your answer: ");
+            String answer = scanner.next();
+            int numAnswer = 0;
+            if (answer.matches("-?\\d+")) {
                 numAnswer = Integer.parseInt(answer);
             }
-            while(rnNumbOne!=0 && rnNumbTwo!=0){
-                if(rnNumbOne>rnNumbTwo)rnNumbOne=rnNumbOne%rnNumbTwo;
-                else rnNumbTwo=rnNumbTwo%rnNumbOne;
-            }max= rnNumbOne+rnNumbTwo;
-            if(numAnswer==max){
+            while (rnNumbOne != 0 && rnNumbTwo != 0) {
+                if (rnNumbOne > rnNumbTwo) {
+                    rnNumbOne = rnNumbOne % rnNumbTwo;
+                } else {
+                    rnNumbTwo = rnNumbTwo % rnNumbOne;
+                }
+            } max = rnNumbOne + rnNumbTwo;
+            if (numAnswer == max) {
                 correctCount++;
                 System.out.println("Correct!");
-            }else {
-                System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was '" + max +"`");
-                System.out.println("Let`s try again, "+ cliName);
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + max + "`");
+                System.out.println("Let`s try again, " + cliName);
                 break;
             }
 
         }
-        if(correctCount==3){
+        if (correctCount == 3) {
             System.out.println("Congratulations, " + cliName + "!");
         }
+
     }
 }
