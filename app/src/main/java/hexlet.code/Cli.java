b9ded970b62709be954a0f8d6b2ba8feb0cbbd54
Ivan Piglov <famenko72@gmail.com>
2024-03-21 21:27:37 +0300

    fix Cli.java

--- app/src/main/java/hexlet.code/Cli.java
+++ app/src/main/java/hexlet.code/Cli.java
@@ -3,12 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void congratCli() {
-        Scanner scanner = new Scanner(System.in);
+    public static void greetCli() {
         System.out.println("May I have your name?");
-        String clientName = scanner.next();
-        System.out.println("Hello, " + clientName + "!");
-        scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        String cliName = scanner.next();
+        System.out.println("Hello, " + cliName + "!");
     }
-
 }
+
