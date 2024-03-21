8cc4fd437c9bf56bc9ac3e22e3d5a22097ee754c
Ivan Piglov <famenko72@gmail.com>
2024-03-21 21:28:54 +0300

    fix CalcGame.java

--- app/src/main/java/hexlet/code/games/CalcGame.java
+++ app/src/main/java/hexlet/code/games/CalcGame.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 
 public class CalcGame {
+
     public void calcGame(String cliName) {
         Random random = new Random();
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int correctCount = 0;
         while (correctCount < 3) {
             int rnOperation = random.nextInt(0, 100);
@@ -15,57 +16,67 @@ public class CalcGame {
             int rnNumbTwo = random.nextInt(0, 10);
             if (rnOperation >= 0 && rnOperation <= 33) {
                 System.out.println("Question: " + rnNumbOne + "-" + rnNumbTwo);
-                String answer =scanner.next();
+                System.out.print("Your answer: ");
+                String answer = scanner.next();
                 int numAnswer = 0;
-                if(answer.matches("-?\\d+")){
-                  numAnswer=Integer.parseInt(answer);
+                if (answer.matches("-?\\d+")) {
+                    numAnswer = Integer.parseInt(answer);
                 }
                 if (rnNumbOne - rnNumbTwo == numAnswer) {
                     correctCount++;
                     System.out.println("Correct");
                 } else {
-                    System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was '" + (rnNumbOne - rnNumbTwo) +"`");
-                    System.out.println("Let`s try again, "+ cliName);
+                    int correctAns = rnNumbOne - rnNumbTwo;
+                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAns + "`");
+                    System.out.println("Let`s try again, " + cliName);
                     break;
                 }
             }
             if (rnOperation > 67) {
                 System.out.println("Question: " + rnNumbOne + "*" + rnNumbTwo);
-                String answer =scanner.next();
+                System.out.print("Your answer: ");
+                String answer = scanner.next();
                 int numAnswer = 0;
-                if(answer.matches("-?\\d+")){
-                    numAnswer=Integer.parseInt(answer);
+                if (answer.matches("-?\\d+")) {
+                    numAnswer = Integer.parseInt(answer);
                 }
                 if (rnNumbOne * rnNumbTwo == numAnswer) {
                     correctCount++;
                     System.out.println("Correct");
                 } else {
-                    System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was '" + (rnNumbOne * rnNumbTwo)+"`");
-                    System.out.println("Let`s try again "+ cliName);
+                    int correctAns = rnNumbOne * rnNumbTwo;
+                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAns + "`");
+                    System.out.println("Let`s try again, " + cliName);
                     break;
                 }
             }
             if (rnOperation >= 34 && rnOperation <= 67) {
                 System.out.println("Question: " + rnNumbOne + "+" + rnNumbTwo);
-                String answer =scanner.next();
+                System.out.print("Your answer: ");
+                String answer = scanner.next();
                 int numAnswer = 0;
-                if(answer.matches("-?\\d+")){
-                    numAnswer=Integer.parseInt(answer);
+                if (answer.matches("-?\\d+")) {
+                    numAnswer = Integer.parseInt(answer);
                 }
                 if (rnNumbOne + rnNumbTwo == numAnswer) {
                     correctCount++;
                     System.out.println("Correct");
                 } else {
-                    System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was '" + (rnNumbOne + rnNumbTwo)+"`");
-                    System.out.println("Let`s try again "+ cliName);
+                    int correctAns = rnNumbOne + rnNumbTwo;
+                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAns + "`");
+                    System.out.println("Let`s try again, " + cliName);
                     break;
                 }
 
             }
+            if (correctCount == 3) {
+                System.out.println("Congratulations, " + cliName + "!");
+            }
 
-        }
-        if (correctCount == 3) {
-            System.out.println("Congratulations, " + cliName + "!");
         }
     }
 }
+
+
+
+
