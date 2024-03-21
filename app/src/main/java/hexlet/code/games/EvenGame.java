a7c2f969057da75a5c53c3e2bbe362f7191cb060
Ivan Piglov <famenko72@gmail.com>
2024-03-21 21:29:07 +0300

    fix EvenGame.java

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -4,24 +4,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class EvenGame {
+
     public void evenGame(String cliName) {
         Random random = new Random();
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int correctCount = 0;
         while (correctCount < 3) {
             int rnNumb = random.nextInt(1, 100);
             System.out.println("Question: " + rnNumb);
+            System.out.print("Your answer: ");
             String answer = scanner.next();
             if (rnNumb % 2 == 0 && answer.equals("yes") || rnNumb % 2 != 0 && answer.equals("no")) {
                 System.out.println("Correct!");
                 correctCount++;
             } else {
-                System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was '" + (rnNumb % 2 == 0 ? "yes" : "no") + "'.");
+                String correctAnswer = (rnNumb % 2 == 0) ? "yes" : "no";
+                System.out.printf("'%s' is a wrong answer ;(. Correct answer was '%s'.", answer, correctAnswer);
+                System.out.println();
+                System.out.println("Let`s try again, " + cliName);
                 break;
             }
-        }
-        if (correctCount == 3) {
-            System.out.println("Congratulations, " + cliName + "!");
         }
     }
 }
