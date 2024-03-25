a8abba4918be295aa755c0fde2f11f1efe8cf5f6
Ivan Piglov <famenko72@gmail.com>
2024-03-25 18:18:22 +0300

    fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("se.patrikerdes.use-latest-versions") version "0.2.18"/*gradle use latest version plugin*/
+    id("se.patrikerdes.use-latest-versions") version "0.2.18" /*gradle use latest version plugin*/
     id("com.github.ben-manes.versions") version "0.51.0"
     application
     checkstyle
@@ -18,20 +18,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 application{
     mainClass="hexlet.code.App"
 }
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
-}
-java
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(20)
-    }
 }
 
 
