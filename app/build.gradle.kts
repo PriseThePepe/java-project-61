7d2bcdc6ba34057d7f3e834f176c49ace73af8d9
Ivan Piglov <famenko72@gmail.com>
2024-03-25 20:55:10 +0300

    fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-}
-
 application{
     mainClass="hexlet.code.App"
 }
