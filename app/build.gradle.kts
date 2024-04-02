dbc1c5a79f82ddad22dfab717c9b47392167173d
Ivan Piglov <famenko72@gmail.com>
2024-04-02 18:29:59 +0300

    fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18" /*gradle use latest version plugin*/
     id("com.github.ben-manes.versions") version "0.51.0"
     application
