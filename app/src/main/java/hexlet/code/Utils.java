faf56ea6480335ab818ee026509e9491221bb4b5
Ivan Piglov <famenko72@gmail.com>
2024-04-02 18:30:40 +0300

    add Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,4 +1,13 @@
 package hexlet.code;
 
+import java.util.Random;
 public class Utils {
+    public static int generateRandomNumber(int bound) {
+        Random random = new Random();
+        return random.nextInt(bound);
+    }
+    public static int generateRandomNumberInRange(int origin, int bound) {
+        Random random = new Random();
+        return random.nextInt(origin, bound);
+    }
 }
