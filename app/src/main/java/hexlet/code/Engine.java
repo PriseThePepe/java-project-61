3eca5a7e90751fe4d8d8bde09fbee998921df827
Ivan Piglov <famenko72@gmail.com>
2024-04-02 18:31:26 +0300

    fix Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,32 +5,21 @@ import java.util.Scanner;
 
 public class Engine {
 
-    private static String cliName;
 
     private static final int QUESTION_ROW_NUMBER = 0;
     private static final int ANSWER_ROW_NUMBER = 1;
 
 
-    public static String getCliName() {
-        return cliName;
-    }
-
-
-
-    public static void saluteCli() {
+    public static void runGame(String mainQuestion, String[][] questionsAnswerPairs) {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
-        cliName = scanner.next();
+        String cliName = scanner.next();
         System.out.println("Hello, " + cliName + "!");
-    }
-    public static void runGame(String mainQuestion, String[][] questionsAnswerPairs) {
-        saluteCli();
         System.out.println(mainQuestion);
-        for (int i = 0; i < questionsAnswerPairs.length; i++) {
-            Scanner scanner = new Scanner(System.in);
-            String correctAnswer = questionsAnswerPairs[i][ANSWER_ROW_NUMBER];
-            System.out.println("Question: " + questionsAnswerPairs[i][QUESTION_ROW_NUMBER]);
+        for (String[] questionsAnswerPair : questionsAnswerPairs) {
+            String correctAnswer = questionsAnswerPair[ANSWER_ROW_NUMBER];
+            System.out.println("Question: " + questionsAnswerPair[QUESTION_ROW_NUMBER]);
             System.out.print("Your answer: ");
             String cliAnswer = scanner.next();
             boolean isAnswerCorrect = cliAnswer.equals(correctAnswer);
